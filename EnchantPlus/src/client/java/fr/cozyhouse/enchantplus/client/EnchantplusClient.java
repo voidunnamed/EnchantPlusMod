ed4e0a8ba5b133ca8ead21715cf832a58b5e4735
Void <84667427+voidunnamed@users.noreply.github.com>
2026-09-06 08:23:59 +0200

    add initial mod

---
+++ EnchantPlus/src/client/java/fr/cozyhouse/enchantplus/client/EnchantplusClient.java
@@ -0,0 +1,10 @@
+package fr.cozyhouse.enchantplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantplusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ EnchantPlus/src/main/java/fr/cozyhouse/enchantplus/Enchantplus.java
@@ -0,0 +1,10 @@
+package fr.cozyhouse.enchantplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Enchantplus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
